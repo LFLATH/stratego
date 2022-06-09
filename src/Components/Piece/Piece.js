73cd1d62abe47bd3def9fcb8aa35872cb30d94e7
LFLATH <ludwiglechtreck@gmail.com>
2022-06-08 23:46:04 -0400

    Currently Working on Displaying images for the correct piece type in the piece tray

--- src/Components/Piece/Piece.js
+++ src/Components/Piece/Piece.js
@@ -1,13 +1,32 @@
 import React, {useState} from "react";
-import bomb from "/Assets";
-
-
+import bomb from "./Assets/bomb.png";
+import captain from "./Assets/captain.png";
+import colonel from "./Assets/colonel.png";
+import flag from "./Assets/flag.png";
+import general from "./Assets/general.png";
+import lieutenant from "./Assets/lieutenant.png";
+import major from "./Assets/major.png";
+import marshal from "./Assets/marshal.png";
+import miner from "./Assets/miner.png";
+import scout from "./Assets/scout.png";
+import sergeant from "./Assets/sergeant.png";
+import spy from "./Assets/spy.png";
 const Piece = ({positionrow, positioncol, color, type}) =>{
     const [isAlive, setMortality] = useState(true)
+    let acitveImage = null;
+    const images = [bomb, captain, colonel, flag, general, lieutenant, major, marshal, miner, scout, sergeant, spy];
+    console.log(String(bomb))
+    images.forEach(element => {
+        if(type === String(element)){
+            acitveImage = element
+        } 
+    });
+    console.log(typeof(acitveImage))
     return(
-            <img className="piece" src={type}/>           
+            <img className="piece" src={acitveImage}/> 
     )
 
 }
 export default Piece;
+
 
